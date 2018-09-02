0081bb0cfc638451285171a3225e02132c205555
gr.istqb@gmail.com <kyriaki02121986>
2018-09-02 18:39:58 +0300

    -

--- src/main/java/page/objects/AmazonCardPage.java
+++ src/main/java/page/objects/AmazonCardPage.java
@@ -1,6 +1,5 @@
 package page.objects;
 
-import com.persado.oss.quality.stevia.selenium.core.SteviaContext;
 import com.persado.oss.quality.stevia.selenium.core.WebComponent;
 
 public class AmazonCardPage extends WebComponent {
@@ -9,8 +8,8 @@ public class AmazonCardPage extends WebComponent {
 
         PROCEED_CHECKOUT_BTN("//*[@id=\"sc-buy-box-ptc-button\"]/span/input"),
         SUBTOTAL("//*[@id=\"gutterCartViewForm\"]/div[3]/div/div/div[1]/p/span/span[2]"),
-        DELETE_PRODUCT("//*[@id=\"activeCartViewForm\"]/div[1]/div[1]/div[2]/div/div/div/div[4]/div/div[1]/div/div/div[2]/div/span[1]/span/input");
-      
+        DELETE_PRODUCT("Delete");
+
         private String myLocator;
 
         AmazonCardPageLocators(String locator) {
@@ -23,8 +22,9 @@ public class AmazonCardPage extends WebComponent {
 
     }
 
-    public void checkPriceInCard(String price) {
-        SteviaContext.verify().equal(price, (controller().getText(AmazonCardPageLocators.SUBTOTAL.get())));
+    public String returnPriceinCard() {
+        String cardPrice = controller().getText(AmazonCardPageLocators.SUBTOTAL.get());
+        return cardPrice;
     }
 
     public void deleteProduct() {
